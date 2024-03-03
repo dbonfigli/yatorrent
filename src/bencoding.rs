918ffb6a8d05f061bdca893ba89276d99ec36797
Diego Bonfigli <d.bonfigli@gmail.com>
2024-03-03 22:05:58 +0100

    bencoded strings are not utf8

--- src/bencoding.rs
+++ src/bencoding.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::str;
 
 type IndexOfError = usize;
 
@@ -20,10 +21,10 @@ pub struct ParseError {
 #[derive(PartialEq, Debug)]
 pub enum Value {
     Error(ParseError),
-    Str(String),
+    Str(Vec<u8>),
     Int(i64),
     List(Vec<Value>),
-    Dict(HashMap<String, Value>),
+    Dict(HashMap<Vec<u8>, Value>),
 }
 
 impl Value {
@@ -31,66 +32,73 @@ impl Value {
         Value::Error(ParseError { elem, index })
     }
 
-    pub fn encode(&self) -> String {
+    pub fn encode(&self) -> Vec<u8> {
         match self {
-            Value::Error(_) => "!!!error!!!".to_string(),
-            Value::Str(string_value) => encode_string(string_value),
-            Value::Int(int_value) => format!("i{}e", int_value),
-            Value::List(list_value) => {
-                let mut list_string = "l".to_string();
-                list_value
-                    .into_iter()
-                    .for_each(|v| list_string = format!("{}{}", list_string, v.encode()));
-                format!("{}e", list_string)
-            }
-            Value::Dict(dict_value) => {
-                let mut dict_string = "d".to_string();
-                dict_value.into_iter().for_each(|entry| {
-                    dict_string = format!(
-                        "{}{}{}",
-                        dict_string,
-                        encode_string(entry.0),
-                        entry.1.encode()
-                    )
-                });
-                format!("{}e", dict_string)
-            }
+            Value::Error(_) => b"!!!error!!!".to_vec(),
+            Value::Str(v) => encode_str(v),
+            Value::Int(v) => encode_int(v),
+            Value::List(v) => encode_list(v),
+            Value::Dict(v) => encode_dict(v),
         }
     }
 
-    pub fn new(source: String) -> Self {
-        let v: Vec<char> = source.chars().collect();
-        from_char_vec(&v, 0).0
+    pub fn new(source: Vec<u8>) -> Self {
+        from_char_vec(&source, 0).0
     }
-
-
 }
 
-fn encode_string(s: &String) -> String {
-    format!("{}:{}", s.len(), s)
+fn encode_dict(d: &HashMap<Vec<u8>, Value>) -> Vec<u8> {
+    let mut v = b"d".to_vec();
+    d.into_iter().for_each(|entry| {
+        v.append(&mut encode_str(entry.0));
+        v.append(&mut entry.1.encode());
+    });
+    v.push(b'e');
+    v
+}
+
+fn encode_list(l: &Vec<Value>) -> Vec<u8> {
+    let mut v = b"l".to_vec();
+    l.into_iter().for_each(|val| v.append(&mut val.encode()));
+    v.push(b'e');
+    v
+}
+
+fn encode_int(i: &i64) -> Vec<u8> {
+    let mut v = b"i".to_vec();
+    v.append(&mut i.to_string().as_bytes().to_vec());
+    v.push(b'e');
+    v
+}
+
+fn encode_str(s: &Vec<u8>) -> Vec<u8> {
+    let mut v = s.len().to_string().as_bytes().to_vec();
+    v.push(b':');
+    v.append(&mut s.clone());
+    v
 }
 
 // source is the source data
 // index is where to look from the source
 // return Value, index of next char to read
-fn from_char_vec(source: &Vec<char>, index: usize) -> (Value, usize) {
+fn from_char_vec(source: &Vec<u8>, index: usize) -> (Value, usize) {
     match source.get(index) {
-        Some('0'..='9') => parse_str(source, index),
-        Some('i') => parse_int(source, index),
-        Some('l') => parse_list(source, index),
-        Some('d') => parse_dict(source, index),
+        Some(b'0'..=b'9') => parse_str(source, index),
+        Some(b'i') => parse_int(source, index),
+        Some(b'l') => parse_list(source, index),
+        Some(b'd') => parse_dict(source, index),
         _ => (Value::new_error(ErrorElem::Unknown, index), index),
     }
 }
 
-fn parse_str(source: &Vec<char>, index: usize) -> (Value, usize) {
+fn parse_str(source: &Vec<u8>, index: usize) -> (Value, usize) {
     let mut index = index;
     let start_string_len_index = index;
     let end_string_len_index;
     loop {
         match source.get(index) {
-            Some('0'..='9') => index += 1,
-            Some(':') => {
+            Some(b'0'..=b'9') => index += 1,
+            Some(b':') => {
                 end_string_len_index = index;
                 index += 1;
                 break;
@@ -98,9 +106,10 @@ fn parse_str(source: &Vec<char>, index: usize) -> (Value, usize) {
             _ => return (Value::new_error(ErrorElem::Str, index), index),
         }
     }
-    let string_len_str: String = source[start_string_len_index..end_string_len_index]
-        .into_iter()
-        .collect();
+    let string_len_str: String =
+        str::from_utf8(&source[start_string_len_index..end_string_len_index])
+            .unwrap()
+            .to_string();
     let string_len_opt = string_len_str.parse::<usize>();
     let string_len;
     match string_len_opt {
@@ -113,7 +122,7 @@ fn parse_str(source: &Vec<char>, index: usize) -> (Value, usize) {
         }
     }
     if string_len == 0 {
-        return (Value::Str("".to_string()), index);
+        return (Value::Str(Vec::new()), index);
     }
     let end_string_index = index + string_len;
     if end_string_index > source.len() {
@@ -122,18 +131,20 @@ fn parse_str(source: &Vec<char>, index: usize) -> (Value, usize) {
             index,
         );
     }
-    let string_value: String = source[index..end_string_index].into_iter().collect();
-    return (Value::Str(string_value), end_string_index);
+    return (
+        Value::Str(source[index..end_string_index].to_vec()),
+        end_string_index,
+    );
 }
 
-fn parse_int(source: &Vec<char>, index: usize) -> (Value, usize) {
+fn parse_int(source: &Vec<u8>, index: usize) -> (Value, usize) {
     let mut index = index + 1;
     let start_int_index = index;
     let end_int_index;
     loop {
         match source.get(index) {
-            Some('0'..='9' | '-') => index += 1,
-            Some('e') => {
+            Some(b'0'..=b'9' | b'-') => index += 1,
+            Some(b'e') => {
                 end_int_index = index;
                 index += 1;
                 break;
@@ -141,7 +152,9 @@ fn parse_int(source: &Vec<char>, index: usize) -> (Value, usize) {
             _ => return (Value::new_error(ErrorElem::Int, index), index),
         }
     }
-    let int_str: String = source[start_int_index..end_int_index].into_iter().collect();
+    let int_str: String = str::from_utf8(&source[start_int_index..end_int_index])
+        .unwrap()
+        .to_string();
 
     // check invalid
     if int_str == "-0" || (int_str.starts_with("0") && int_str.len() > 1) {
@@ -159,13 +172,13 @@ fn parse_int(source: &Vec<char>, index: usize) -> (Value, usize) {
     }
 }
 
-fn parse_list(source: &Vec<char>, index: usize) -> (Value, usize) {
+fn parse_list(source: &Vec<u8>, index: usize) -> (Value, usize) {
     let mut l = Vec::new();
     let mut index = index + 1;
     loop {
         match source.get(index) {
             None => return (Value::new_error(ErrorElem::List, index), index),
-            Some('e') => {
+            Some(b'e') => {
                 index += 1;
                 break;
             }
@@ -183,13 +196,13 @@ fn parse_list(source: &Vec<char>, index: usize) -> (Value, usize) {
     (Value::List(l), index)
 }
 
-fn parse_dict(source: &Vec<char>, index: usize) -> (Value, usize) {
+fn parse_dict(source: &Vec<u8>, index: usize) -> (Value, usize) {
     let mut d = HashMap::new();
     let mut index = index + 1;
     loop {
         match source.get(index) {
             None => return (Value::new_error(ErrorElem::Dict, index), index),
-            Some('e') => {
+            Some(b'e') => {
                 index += 1;
                 break;
             }
@@ -223,29 +236,29 @@ mod tests {
     #[test]
     fn encode_value() {
         let val_l = Value::List(vec![
-            Value::Dict(HashMap::from([("k1".to_string(), Value::Int(1))])),
+            Value::Dict(HashMap::from([(b"k1".to_vec(), Value::Int(1))])),
             Value::Int(2),
             Value::Int(3),
-            Value::Str("bye".to_string()),
+            Value::Str(b"bye".to_vec()),
         ]);
-        assert_eq!("ld2:k1i1eei2ei3e3:byee".to_string(), val_l.encode());
+        assert_eq!(b"ld2:k1i1eei2ei3e3:byee".to_vec(), val_l.encode());
     }
 
     #[test]
     fn decode_int() {
-        assert_eq!(Value::new("i2e".to_string()), Value::Int(2));
-        assert_eq!(Value::new("i23e".to_string()), Value::Int(23));
-        assert_eq!(Value::new("i-2312e".to_string()), Value::Int(-2312));
-        assert_eq!(Value::new("i0e".to_string()), Value::Int(0));
+        assert_eq!(Value::new(b"i2e".to_vec()), Value::Int(2));
+        assert_eq!(Value::new(b"i23e".to_vec()), Value::Int(23));
+        assert_eq!(Value::new(b"i-2312e".to_vec()), Value::Int(-2312));
+        assert_eq!(Value::new(b"i0e".to_vec()), Value::Int(0));
         assert_eq!(
-            Value::new("i-0e".to_string()),
+            Value::new(b"i-0e".to_vec()),
             Value::Error(ParseError {
                 elem: ErrorElem::Int,
                 index: 1
             })
         );
         assert_eq!(
-            Value::new("i01e".to_string()),
+            Value::new(b"i01e".to_vec()),
             Value::Error(ParseError {
                 elem: ErrorElem::Int,
                 index: 1
@@ -256,12 +269,12 @@ mod tests {
     #[test]
     fn decode_str() {
         assert_eq!(
-            Value::new("5:hello".to_string()),
-            Value::Str("hello".to_string())
+            Value::new(b"5:hello".to_vec()),
+            Value::Str(b"hello".to_vec())
         );
-        assert_eq!(Value::new("0:".to_string()), Value::Str("".to_string()));
+        assert_eq!(Value::new(b"0:".to_vec()), Value::Str(b"".to_vec()));
         assert_eq!(
-            Value::new("6:hello".to_string()),
+            Value::new(b"6:hello".to_vec()),
             Value::Error(ParseError {
                 elem: ErrorElem::Str,
                 index: 0
@@ -271,61 +284,58 @@ mod tests {
 
     #[test]
     fn decode_list() {
-        let val_l = Value::List(vec![Value::Str("bye".to_string())]);
-        assert_eq!(Value::new("l3:byee".to_string()), val_l);
+        let val_l = Value::List(vec![Value::Str(b"bye".to_vec())]);
+        assert_eq!(Value::new(b"l3:byee".to_vec()), val_l);
     }
 
     #[test]
     fn decode_list2() {
         let val_l = Value::List(vec![
-            Value::Str("bye".to_string()),
-            Value::Str("hello".to_string()),
+            Value::Str(b"bye".to_vec()),
+            Value::Str(b"hello".to_vec()),
         ]);
-        assert_eq!(Value::new("l3:bye5:helloe".to_string()), val_l);
+        assert_eq!(Value::new(b"l3:bye5:helloe".to_vec()), val_l);
     }
 
     #[test]
     fn decode_list3() {
         let val_l = Value::List(vec![
-            Value::Dict(HashMap::from([("k1".to_string(), Value::Int(1))])),
+            Value::Dict(HashMap::from([(b"k1".to_vec(), Value::Int(1))])),
             Value::Int(2),
             Value::Int(3),
-            Value::Str("bye".to_string()),
+            Value::Str(b"bye".to_vec()),
         ]);
-        assert_eq!(Value::new("ld2:k1i1eei2ei3e3:byee".to_string()), val_l);
+        assert_eq!(Value::new(b"ld2:k1i1eei2ei3e3:byee".to_vec()), val_l);
     }
 
     #[test]
     fn decode_list4() {
         let val_l = Value::List(vec![Value::Int(2), Value::Int(3), Value::Int(-3)]);
-        assert_eq!(Value::new("li2ei3ei-3ee".to_string()), val_l);
+        assert_eq!(Value::new(b"li2ei3ei-3ee".to_vec()), val_l);
     }
 
     #[test]
     fn decode_hash() {
         let val_l = Value::Dict(HashMap::from([
-            ("k1".to_string(), Value::Str("e2".to_string())),
-            ("k3".to_string(), Value::Str("e3".to_string())),
+            (b"k1".to_vec(), Value::Str(b"e2".to_vec())),
+            (b"k3".to_vec(), Value::Str(b"e3".to_vec())),
         ]));
-        assert_eq!(Value::new("d2:k12:e22:k32:e3e".to_string()), val_l);
+        assert_eq!(Value::new(b"d2:k12:e22:k32:e3e".to_vec()), val_l);
     }
 
     #[test]
     fn decode_hash2() {
         let val_l = Value::Dict(HashMap::from([
             (
-                "k1".to_string(),
+                b"k1".to_vec(),
                 Value::List(vec![
                     Value::Int(0),
-                    Value::Str("hello".to_string()),
-                    Value::Str("".to_string()),
+                    Value::Str(b"hello".to_vec()),
+                    Value::Str(b"".to_vec()),
                 ]),
             ),
-            ("k2".to_string(), Value::Str("e3".to_string())),
+            (b"k2".to_vec(), Value::Str(b"e3".to_vec())),
         ]));
-        assert_eq!(
-            Value::new("d2:k1li0e5:hello0:e2:k22:e3e".to_string()),
-            val_l
-        );
+        assert_eq!(Value::new(b"d2:k1li0e5:hello0:e2:k22:e3e".to_vec()), val_l);
     }
 }
