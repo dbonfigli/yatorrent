e0a9d5ff6200c3c041aae230492612048c13c281
Diego Bonfigli <d.bonfigli@gmail.com>
2024-08-16 19:15:24 +0200

    allow to define listening port

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // get command parameters
     let args: Vec<_> = env::args().collect();
     if args.len() < 2 {
-        println!("Must include the name of a .torrent file as first argument, second argument is optional and is the base path where the files will be downloaded");
+        println!("Must include the name of a .torrent file as first argument\nsecond argument is optional and is the base path where the files will be downloaded\n third argument is optional and is the listening port (default: 8000)");
         exit(1);
     }
     let torrent_file = args.get(1).unwrap();
@@ -39,6 +39,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         Some(p) => Path::new(p),
         None => current_path.as_path(),
     };
+    let port = match args.get(3) {
+        Some(p) => p
+            .parse::<i32>()
+            .expect("could not convert port argument to integer"),
+        None => 8000,
+    };
 
     let contents = fs::read(torrent_file).unwrap();
     let torrent_content = bencoding::Value::new(&contents);
@@ -46,7 +52,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     match metainfo {
         Ok(m) => {
             log::info!("torrent file metainfo:\n{}", m);
-            let mut torrent_manager = TorrentManager::new(base_path, 8000, m).await?;
+            let mut torrent_manager = TorrentManager::new(base_path, port, m).await?;
             torrent_manager.start().await;
             exit(0);
         }
